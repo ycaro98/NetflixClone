55c9ddd359f661aee401ded7784703eba9f68b14
ycaro98 <ycarus.music@gmail.com>
2022-12-11 19:18:18 -0300

    adicionar projeto

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let  api_key = "9d37357566c81f6a0b29fc6754130402"
+
+let img_url = "https://image.tmdb.org/t/p/w500"
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
---
+++ public/js/scroll.js
@@ -0,0 +1,20 @@
+const SetupScrolling = () =>{
+    const container = [...document.querySelectorAll
+    (".movie-container")]
+
+    const nextBtn = [...document.querySelectorAll (".next-btn")]
+    const prevBtn = [...document.querySelectorAll (".pre-btn")]
+
+    container.forEach((item, i)=>{
+        let containerDimensions = item.getBoundClientRect()
+        let containerWidth = containerDimensions.width
+
+        nextBtn[i].addEventListener('click', ()=>{
+            item.scrollLeft += containerWidth
+        })
+
+        prevBtn[i].addEventListener('click', ()=>{
+            item.scrollLeft -= containerWidth
+        })
+    })
+}
